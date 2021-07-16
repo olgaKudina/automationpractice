21b26060ea8cb749351ad335487d22a3c8468f7b
Olga Kudina <olga.kudzina@gmail.com>
2021-07-16 10:54:10 +0300

    Add gitignore file

--- src/main/java/pages/MyAccountPage.java
+++ src/main/java/pages/MyAccountPage.java
@@ -7,8 +7,8 @@ import org.openqa.selenium.WebDriver;
 
 @Log4j2
 public class MyAccountPage extends BasePage {
-    public static final By MY_ACCOUNT_NAVIGATE_PAGE = By.xpath("//span[contains(text(),'My account')]");
-    public static final By AUTHENTICATION_NAVIGATE_PAGE = By.xpath("//*[contains(text(),'Authentication')]");
+    public static final By MY_ACCOUNT_NAVIGATE_XPATH = By.xpath("//span[contains(text(),'My account')]");
+    public static final By AUTHENTICATION_NAVIGATE_XPATH = By.xpath("//*[contains(text(),'Authentication')]");
 
     public MyAccountPage(WebDriver driver) {
         super(driver);
@@ -16,11 +16,11 @@ public class MyAccountPage extends BasePage {
 
     @Step("Verify my Account Navigate Page is displayed after successful Login")
     public boolean isMyAccountNavigatePageDisplayed() {
-        return driver.findElement(MY_ACCOUNT_NAVIGATE_PAGE).isDisplayed();
+        return driver.findElement(MY_ACCOUNT_NAVIGATE_XPATH).isDisplayed();
     }
 
     @Step("Verify Authentication Navigate Page is displayed after Logout")
     public boolean isAuthenticationNavigatePageDisplayed() {
-        return driver.findElement(AUTHENTICATION_NAVIGATE_PAGE).isDisplayed();
+        return driver.findElement(AUTHENTICATION_NAVIGATE_XPATH).isDisplayed();
     }
 }
