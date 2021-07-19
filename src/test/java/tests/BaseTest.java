f2c57985b96b33cb598f8ff329dbc57a8f7faf58
Olga Kudina <olga.kudzina@gmail.com>
2021-07-19 19:16:07 +0300

    Add quit test

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 import steps.AddProductsToCartSteps;
@@ -60,8 +61,8 @@ public class BaseTest {
 
     }
 
-//    @AfterMethod
-//    public void endTest() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void endTest() {
+        driver.quit();
+    }
 }
